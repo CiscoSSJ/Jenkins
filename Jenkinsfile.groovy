55710373b473eb064fa313cd643a986a522aa985
Francisco-Diaz-Pozuelo <Francisco.Diaz.Pozuelo@ocean.ibm.com>
2023-01-26 15:38:55 +0100

    Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,41 @@
+podTemplate(inheritFrom: 'icagent01') {
+  node(POD_LABEL) {
+    stage ('git checkout'){
+        steps.echo ":: Checking out code"
+		steps.checkout( [ $class: 'GitSCM',
+                  branches: [ [ name: "${branch}" ] ],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [],
+                  gitTool: 'Default',
+                  submoduleCfg: [],
+                  userRemoteConfigs: [ [ credentialsId: "", url: "${params.repository}" ] ]
+                ] )
+		steps.echo "Getting GIT_COMMIT..."
+		sh 'git rev-parse HEAD > commit'
+        def commit = readFile('commit').trim()
+        env.GIT_COMMIT = commit
+        echo "GIT_COMMIT: ${env.GIT_COMMIT}"
+    }
+    stage('Info') {
+        container('jenkins-slave') {
+            sh 'pwd'
+            sh 'ls'
+        }
+    }
+    stage('Build') {
+        container('maven') {
+            sh 'mvn -B -DskipTests clean package'
+        }
+    }
+    stage('Test') {
+        container('maven') {
+            sh 'mvn test'
+        }
+    }
+    stage('Kubectl') {
+        container('kubectl') {
+            sh 'kubectl --version'
+        }
+    }
+  }
+}
