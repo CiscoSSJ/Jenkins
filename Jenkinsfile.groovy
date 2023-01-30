a52f34fd14a7ca1c2669d0d9b6e01a12fcea2bea
Francisco Diaz Pozuelo <98550981+CiscoSSJ@users.noreply.github.com>
2023-01-30 08:32:05 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,28 +1,30 @@
 podTemplate(inheritFrom: 'icagent01') {
   node(POD_LABEL) {
     stage('Clean Workspace') {
-        container('jenkins-slave') {
+        container('jnlp') {
             cleanWs()
         }
     }
     stage ('git checkout'){
-        steps.echo ":: Checking out code"
-		steps.checkout( [ $class: 'GitSCM',
-                  branches: [ [ name: "${branch}" ] ],
-                  doGenerateSubmoduleConfigurations: false,
-                  extensions: [],
-                  gitTool: 'Default',
-                  submoduleCfg: [],
-                  userRemoteConfigs: [ [ credentialsId: "", url: "${params.repository}" ] ]
-                ] )
-		steps.echo "Getting GIT_COMMIT..."
-		sh 'git rev-parse HEAD > commit'
-        def commit = readFile('commit').trim()
-        env.GIT_COMMIT = commit
-        echo "GIT_COMMIT: ${env.GIT_COMMIT}"
+	container('git') {
+		steps.echo ":: Checking out code"
+			steps.checkout( [ $class: 'GitSCM',
+			  branches: [ [ name: "${branch}" ] ],
+			  doGenerateSubmoduleConfigurations: false,
+			  extensions: [],
+			  gitTool: 'Default',
+			  submoduleCfg: [],
+			  userRemoteConfigs: [ [ credentialsId: "", url: "${params.repository}" ] ]
+			] )
+			steps.echo "Getting GIT_COMMIT..."
+			sh 'git rev-parse HEAD > commit'
+		def commit = readFile('commit').trim()
+		env.GIT_COMMIT = commit
+		echo "GIT_COMMIT: ${env.GIT_COMMIT}"
+	}
     }
     stage('Info') {
-        container('jenkins-slave') {
+        container('jnlp') {
             sh 'pwd'
             sh 'ls'
         }
